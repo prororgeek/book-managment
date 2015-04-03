6e3a0441ef7cab5d2d4cbbef5997cc4397455a99
Ravi Jain <phpabby@gmail.com>
2015-04-03 16:44:29 +0530

    Changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(  :email, :name , :image, :mobile, :gender, :address, :password, :password_confirmation, :remember_me, :role_id ) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(  :email, :name, :mobile, :gender, :address, :password, :password_confirmation, :current_password, :role_id ) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(  :email, :name , :about, :password, :password_confirmation, :remember_me, :role_id ) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(  :email, :name, :about, :password, :password_confirmation, :current_password, :role_id ) }
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402163101) do
+ActiveRecord::Schema.define(version: 20150403064840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20150402163101) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "role_id"
+    t.string   "name"
+    t.text     "about"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
